5459230e166e224e38998dba5072e71318be6fb7
Nicolas Sroczynski <nicolas.sroczynski@gmail.com>
2023-07-24 12:24:35 -0300

    Initial Commit

---
+++ ClientSocket/Program.cs
@@ -0,0 +1,53 @@
+﻿using System.Net.Sockets;
+using System.Text;
+
+string serverIp = "127.0.0.1";
+int port = 8080;
+
+try
+{
+    // Create a TCP/IP socket
+    TcpClient client = new TcpClient();
+
+    // Connect to the server
+    client.Connect(serverIp, port);
+    Console.WriteLine("Connected to server.");
+
+    // Get the network stream from the server
+    NetworkStream stream = client.GetStream();
+
+    // Start a continuous chat loop
+    while (true)
+    {
+        // Prompt for a client message
+        Console.Write("Client: ");
+        string clientMessage = Console.ReadLine();
+
+        // Send the client message to the server
+        byte[] clientMessageBytes = Encoding.ASCII.GetBytes(clientMessage);
+        stream.Write(clientMessageBytes, 0, clientMessageBytes.Length);
+        Console.WriteLine("Sent message: " + clientMessage);
+
+        // Receive the response from the server
+        byte[] buffer = new byte[1024];
+        int bytesRead = stream.Read(buffer, 0, buffer.Length);
+        string response = Encoding.ASCII.GetString(buffer, 0, bytesRead);
+        Console.WriteLine("Server: " + response);
+
+        // Check if the server wants to end the chat
+        if (response.Equals("Goodbye!", StringComparison.OrdinalIgnoreCase))
+        {
+            break;
+        }
+    }
+
+    // Close the connection
+    client.Close();
+}
+catch (Exception ex)
+{
+    Console.WriteLine("Error: " + ex.Message);
+}
+
+Console.WriteLine("Press any key to exit.");
+Console.ReadKey();
---
+++ ServerSocket/Program.cs
@@ -0,0 +1,63 @@
+﻿using System.Net.Sockets;
+using System.Net;
+using System.Text;
+
+string ipAddress = "127.0.0.1";
+int port = 8080;
+
+TcpListener listener = new TcpListener(IPAddress.Parse(ipAddress), port);
+
+try
+{
+    listener.Start();
+    Console.WriteLine("Server started. Waiting for clients...");
+
+    TcpClient client = listener.AcceptTcpClient();
+    Console.WriteLine("Client connected.");
+
+    // Get the network stream from the client
+    NetworkStream stream = client.GetStream();
+
+    // Start a continuous chat loop
+    while (true)
+    {
+        // Receive the message from the client
+        byte[] buffer = new byte[1024];
+        int bytesRead = stream.Read(buffer, 0, buffer.Length);
+        string message = Encoding.ASCII.GetString(buffer, 0, bytesRead);
+        Console.WriteLine("Client: " + message);
+
+        // Check if the client wants to end the chat
+        if (message.Equals("bye", StringComparison.OrdinalIgnoreCase))
+        {
+            // Send a goodbye message and close the connection
+            string response = "Goodbye!";
+            byte[] responseBytes = Encoding.ASCII.GetBytes(response);
+            stream.Write(responseBytes, 0, responseBytes.Length);
+            Console.WriteLine("Sent response: " + response);
+            break;
+        }
+
+        // Prompt for a server message
+        Console.Write("Server: ");
+        string serverMessage = Console.ReadLine();
+
+        // Send the server message to the client
+        byte[] serverMessageBytes = Encoding.ASCII.GetBytes(serverMessage);
+        stream.Write(serverMessageBytes, 0, serverMessageBytes.Length);
+        Console.WriteLine("Sent message: " + serverMessage);
+    }
+
+    client.Close();
+}
+catch (Exception ex)
+{
+    Console.WriteLine("Error: " + ex.Message);
+}
+finally
+{
+    listener.Stop();
+}
+
+Console.WriteLine("Server stopped. Press any key to exit.");
+Console.ReadKey();
